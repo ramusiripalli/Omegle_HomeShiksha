56efc94dff9f6c93523bc8e5b42eb0e455f9bc0d
ramu siripalli <ramsiripalli2772@gmail.com>
2024-07-21 12:48:54 +0530

    chnages in package.json
    in package.json I have created npm run dev , which runs build and start script t once

--- frontEnd_Omegle/src/App.tsx
+++ frontEnd_Omegle/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { Landing } from './components/Landing';
 import { Room } from './components/Room';
+import { io } from 'socket.io-client';
 
 function App() {
 
