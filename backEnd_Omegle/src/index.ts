3f4396adb716e140d7c2fbdbabdecd38fdbf0437
ramu siripalli <ramsiripalli2772@gmail.com>
2024-07-21 09:51:14 +0530

    added backend socket.io
    added js file

--- backEnd_Omegle/src/index.ts
+++ backEnd_Omegle/src/index.ts
@@ -1,19 +1,15 @@
+import { Socket } from "socket.io";
+import http from "http";
 const express = require('express');
-import { createServer } from 'node:http';
-import { join } from 'node:path';
-import { Server } from 'socket.io';
-
+const { Server } = require('socket.io');
 const app = express();
-const io = new Server(app);
+const server = http.createServer(http);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
+io.on('connection', (socket:Socket) => {
   console.log('a user connected');
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
