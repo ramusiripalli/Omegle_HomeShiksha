a2f4b9b0fedaad0f61c44fb8a9353622456f9d8d
ramu siripalli <ramsiripalli2772@gmail.com>
2024-07-21 09:28:33 +0530

    Backend Basics
    Backend basics added

--- backEnd_Omegle/src/index.ts
+++ backEnd_Omegle/src/index.ts
@@ -1,3 +1,19 @@
-console.log("Hello setting up assignment");
+const express = require('express');
+import { createServer } from 'node:http';
+import { join } from 'node:path';
+import { Server } from 'socket.io';
 
-console.log("Welcome to omegle code writing ")
+const app = express();
+const io = new Server(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
